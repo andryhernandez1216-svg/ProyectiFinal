5034d2bfaf95ad89d9dce17294d2164fb5c67188
edualizagomez <edualizagomez@192.168.1.8>
2026-03-25 23:16:32 -0400

    Creando clase Usuario

--- Altice/src/Ligca/Usuario.java
+++ Altice/src/Ligca/Usuario.java
@@ -1,5 +1,20 @@
 package Ligca;
 
 public class Usuario {
+	
+	private String rol;
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	public Usuario(String rol) {
+		super();
+		this.rol = rol;
+	}
 
 }
