c3f9052dae5878f8bea80a7ad612c122a0b9130b
edualizagomez <edualizagomez@192.168.1.12>
2026-03-27 23:04:44 -0400

    Actualizacion

--- Altice/src/Ligca/SistemaAltice.java
+++ Altice/src/Ligca/SistemaAltice.java
@@ -13,39 +13,27 @@ public class SistemaAltice {
 	public ArrayList<Cliente> getClientes() {
 		return clientes;
 	}
-	public void setClientes(ArrayList<Cliente> clientes) {
-		this.clientes = clientes;
-	}
+	
 	public ArrayList<Empleado> getEmpleados() {
 		return empleados;
 	}
-	public void setEmpleados(ArrayList<Empleado> empleados) {
-		this.empleados = empleados;
-	}
+	
 	public ArrayList<Plan> getPlanes() {
 		return planes;
 	}
-	public void setPlanes(ArrayList<Plan> planes) {
-		this.planes = planes;
-	}
+	
 	public ArrayList<Contrato> getContratos() {
 		return contratos;
 	}
-	public void setContratos(ArrayList<Contrato> contratos) {
-		this.contratos = contratos;
-	}
+	
 	public ArrayList<Pago> getPagos() {
 		return pagos;
 	}
-	public void setPagos(ArrayList<Pago> pagos) {
-		this.pagos = pagos;
-	}
+
 	public ArrayList<Servicio> getServicios() {
 		return servicios;
 	}
-	public void setServicios(ArrayList<Servicio> servicios) {
-		this.servicios = servicios;
-	}
+	
 	public SistemaAltice(ArrayList<Cliente> clientes, ArrayList<Empleado> empleados, ArrayList<Plan> planes,
 			ArrayList<Contrato> contratos, ArrayList<Pago> pagos, ArrayList<Servicio> servicios) {
 		super();
